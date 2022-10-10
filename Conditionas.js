5ec01616ee257d634e15033203ddba195a5e72bf
lvlada <vladimirlukic197@gmail.com>
2022-10-10 22:08:48 +0200

    JS

---
+++ Conditionas.js
@@ -0,0 +1,148 @@
+//1
+
+// var a = 3;
+// var b = -7;
+// var c = 2;
+
+// if (a < 0){
+//     console.log('The sign is ' + a);
+// }
+// else if (b < 0){
+//     console.log('The sign is ' + b);
+// } else{ 
+//     console.log('The sign is ' + c);
+// }
+
+//2
+
+// var a = -5;
+// var b = -2;
+// var c = -6;
+// var e = 0;
+// var f = -1;
+// var rez = a;
+
+// if ( rez < b && b > c && b > e && b > f){
+//     rez = b;
+// }
+// if ( rez < c && c > b && c > e && c > f){
+//     rez = c;
+// }
+// if ( rez < e && e > b && e > c && e > f){
+//     rez = e;
+// }
+// if ( rez < f && f > b && f > c && f > e){
+//     rez = f;
+// }
+
+// console.log(rez);
+
+//3
+//  var a = 0;
+//  var b = -1;
+//  var c = 4;
+
+//  if (a > b &&  a > c){
+//     if (b > c){
+//         console.log(a, b, c);
+//     } else {console.log(a, c, b);}
+//  } else if (b > a && b > c){
+//     if (a > c){
+//         console.log(b, a, c);
+//     } else{
+//         console.log(b, c, a);
+//     }
+//  } else if (c > a && c > b){
+//     if(a > b){
+//         console.log(c, a, b);
+//     } else{
+//         console.log(c, b, a);
+//     }
+//  }
+
+ //4
+//  var x = 10;
+
+//  if (!isNaN(x)){
+//     if( x % 2 == 0){
+//         console.log(x + '/2 = '+ x/2  );
+//     }else {
+//         console.log('X');
+//     }
+//  }
+
+//  //5
+
+//  var t = 5;
+//  var n = 10;
+
+//  if (t > n) {
+
+//     console.log(t);
+//  } else{ console.log(n) }
+
+ //6
+
+//  var c = 60;
+//  var f;
+//  f = (9*c/5) + 32;
+
+//  console.log(c + 'C' + ' is ' + f + 'F');
+
+ //7
+ 
+//  var number = 15;
+//  var rez;
+
+
+//  if (number > 13) {
+//     rez = (number - 13) * 2;
+
+//  } else{
+//     rez = 13 - number
+//  }
+
+//  console.log(rez);
+
+ //8
+
+//  var num1 = 7;
+//  var num2 = 7;
+//  var rez;
+
+//  if( num1 == num2){
+//     rez = (num1 + num2) * 3;
+
+//  } else{
+
+//     rez = num1 + num2;
+//  }
+//  console.log(rez);
+
+ //9
+
+// var num1 = 6;
+// var num2 = 50;
+// var rez;
+
+// if(num1 == 50 || num2 == 50 || num1 + num2 == 50){
+
+//     rez = true;
+// } else{
+//     rez = false;
+// }
+// console.log(rez);
+
+//10
+
+// var num = 99;
+// var rez = '';
+
+
+// if (num >= 20 && num <=100){
+//     rez = '20 <=> 100';
+// } else if (num > 100 && num <= 400){
+//     rez = '100 => 400';
+// } 
+
+// console.log(rez);
---
+++ variable.js
@@ -0,0 +1,78 @@
+// Variable declaration and initialization
+
+// var firstBox = 4 + 2;
+// var secBox = 5;
+// var firstBox2 = "prvi";
+// var secBox2 = "drugi";
+// console.log(firstBox, secBox, secBox2, firstBox2);
+
+//Arithmetic operators
+
+
+// var banana = 1.2;
+// var banana_gr = banana * 1000
+
+// console.log(banana_gr + ' grama');
+
+// var Anna;
+// var Tom;
+// Anna = 5 * 60;
+// Tom = 30 * 60;
+// console.log('Anna late ' + Anna + ' seconds');
+// console.log('Tom late ' + Tom + ' seconds');
+
+// var usb;
+// usb = 4 * 1024;
+// articles = usb / 96;
+// console.log(articles);
+
+// var a = 1;
+// a = a * 2;
+// //a *= 2;
+// console.log(a);
+
+
+//10
+// var name = 'Vladimir';
+// var age = 47;
+// var cat = false
+
+// console.log(typeof name, typeof age, typeof cat);
+
+// //11
+
+// console.log(typeof 'number', typeof true, typeof null, typeof 'false', typeof 56);
+
+
+//14
+
+// var age = 0;
+
+// if(age < 0 || age == 0){
+//     console.log('Uneo si negativnu vrednost ili vrednost 0');
+// } else if(age > 120){
+
+//     console.log('Tvoja uneta vrednost je veca od 120');
+// } else{
+//     console.log('Ti imas ' + age);
+// }
+
+//15
+
+// var speed = 60;
+
+// if (speed <= 0){
+//     console.log('Trenutno tvoj auto stoji ili si uneo pogresne vrednosti');
+// } 
+// else if(speed < 60 || speed > 120){
+//     console.log(false)
+// }else{
+//     console.log(true)
+// }
+
+
+
+
+
+
+
