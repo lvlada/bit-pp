7c31b784b233244c95ee888dfdfa29f271a2bd3f
lvlada <vladimirlukic197@gmail.com>
2022-10-15 11:27:33 +0200

    ver1

---
+++ Conditionals2.js
@@ -0,0 +1,133 @@
+//1
+// var a = 3;
+// var b = 3;
+// var c = 3;
+
+// if (a == b && b == c && c == a){
+//     console.log('This is equilateral triangle.');
+// } else if (a != b && b != c && c != a){
+//     console.log('This is scalene triangle.');
+// }else{
+//     console.log('This is isosceles triangle.')
+// }
+
+
+//2
+// var numb1 =7;
+// var numb2 = 5;
+// var rez = 0;
+// var operation = 'add';
+
+// if(operation == 'add') {
+//     rez = numb1 + numb2;
+//     console.log('Operation add of two numbers is ' + rez);
+// }
+// if(operation == 'subtract') {
+//     rez = numb1 - numb2;
+//     console.log('Operation subtract of two numbers is ' + rez);
+// }
+// if(operation == 'multiply') {
+//     rez = numb1 * numb2;
+//     console.log('Operation multiply of two numbers is ' + rez);
+// }
+// if(operation == 'divide') {
+//     rez = numb1 / numb2;
+//     console.log('Operation divide of two numbers is ' + rez);
+// }
+// if(operation == 'modulus') {
+//     rez = numb1 % numb2;
+//     console.log('Operation modulus of two numbers is ' + rez);
+// }
+
+
+//3
+
+// var grade = '';
+// var score = 101;
+
+// if (score >= 0 && score <=50){
+//     console.log('Sorry, you faild!');
+// } else if (score > 50 && score <= 60){
+//     grade = 'E'
+//     console.log('Your grade is ' + grade);
+// }else if (score > 60 && score <= 70){
+//     grade = 'D'
+//     console.log('Your grade is ' + grade);
+// }
+// else if (score > 70 && score <= 80){
+//     grade = 'C'
+//     console.log('Your grade is ' + grade);
+// }
+// else if (score > 80 && score <= 90){
+//     grade = 'B'
+//     console.log('Your grade is ' + grade);
+// }else if (score > 90 && score <= 100){
+//     grade = 'A'
+//     console.log('Your grade is ' + grade);
+// } else{
+//     console.log('Your grade number is wrong!');
+// }
+
+
+//4
+
+// var number = 1;
+// var noun = 'cat';
+
+// if(number <= 1){
+//     console.log(number + ' ' + noun);
+// } else if (number > 1){
+//     if(noun != 'gees'){
+//         console.log(number + ' ' + noun + 's')
+//     } else if( noun == 'gees'){
+//         console.log(number + ' ' + noun + 'e')
+//     }
+        
+// }
+
+
+//5
+// var age = 15;
+// var diff = 0
+// if(age >= 18){
+//     console.log('You are old enough to drive');
+// } 
+// if(age < 18){
+//     diff = 18 - age
+//     if(diff == 1){
+//         console.log('You are ' + age + ' old and you need ' + diff + ' year for drive licence' );
+//     } else{
+//         console.log('You are ' + age + ' old and you need ' + diff + ' years for drive licence' );
+//     }
+// }
+
+
+// var myAge = 47;
+// var diff = 0;
+// if(myAge > age){
+//     diff = myAge - age;
+//     console.log('He/She is ' + diff + ' years younger than me.');
+
+// }  else if (myAge == age){
+//     console.log('We are the same age!');
+
+// } else if (myAge < age){
+//     diff = age - myAge;
+//     console.log('He/She is ' + diff + ' year/\s older than me.');
+// }
+
+//7
+
+// var canSpecie1 = 'staford';
+// var canSpecie2 = 'pitbul';
+// var canSpecie3 = 'jazavicar';
+// var canSpecie4 = 'dingoes';
+// var canSpecie5 = 'haski';
+// var myCan = 'haski';
+
+// if(myCan == canSpecie5){
+//     console.log('This is ' + myCan + ' called Zevs!');
+// } else{
+//     console.log('This is a mixed breed.')
+// }
+
