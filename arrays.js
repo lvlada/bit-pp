1e705dae74eaa025e97ebec9f4c00f4bd1343caa
lvlada <vladimirlukic197@gmail.com>
2022-10-18 18:49:19 +0200

    ver1

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var a = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'avg', 'sep', 'oct', 'nov', 'dec'];
+console.log(a[5], a[9], a[0]);
+
+var b = ['pon', 'utor', 'sre', 'cet', 'pet', 'sub', 'ned'];
+console.log(b[6]);
+
+var d = [2, -4, 5, -2, -11];
+console.log(d[1], d[3], d[4]);
+
+var c = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+console.log(c[1], c[4], c[5], c[6], c[8]);
+
+var e = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3,10, 26, 7]];
+console.log(e[1][3]);
+console.log(e[2], e[2][1]);
