7f63ef972279a1c7a0fe76c50788876475918888
lvlada <vladimirlukic197@gmail.com>
2022-10-18 19:39:38 +0200

    ver1

--- arrays.js
+++ arrays.js
@@ -13,3 +13,8 @@ console.log(c[1], c[4], c[5], c[6], c[8]);
 var e = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3,10, 26, 7]];
 console.log(e[1][3]);
 console.log(e[2], e[2][1]);
+
+// Test with variables in arrays
+var testName = "vladimir";
+var niz = [1, testName, 2];
+console.log(niz[1]);
