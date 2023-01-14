a04193773a5884860c671c02acfc19f1ffbc3efb
lvlada <vladimirlukic197@gmail.com>
2023-01-14 10:47:02 +0100

    ver1

---
+++ BIG_Exercises/festival.js
@@ -0,0 +1,51 @@
+'stric mode';
+
+(function(){
+
+    function Genre(name){
+        this.name = name,
+        this.getData = function(){
+
+        }
+    }
+
+    function Movie(genre){
+        this.title = title,
+        this.genre = genre,
+        this.length = this.length,
+        this.getData = function(){
+
+        }
+
+    }
+
+    function Program(date){
+        this.date = new Date(date),
+        this.movies = [],
+        this.totalMovievs = this.movies.length,
+        this.addMovie = function(){
+
+        },
+
+        this.addData = function(){
+            //return a formatted string of all data related to the program
+
+            
+        }
+    }
+
+    function Festival(name){
+        this.name = name,
+        this.programs = [],
+        this.moviePrograms = this.programs.length,
+        this.addProgram = function(){
+
+        }
+    }
+
+
+    
+
+
+
+})();
